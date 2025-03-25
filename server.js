c57a1e2bac20729878f0e8c64baecc1435f5e48c
Kurt <schubert.geschaeftlich@gmail.com>
2025-03-25 16:03:24 +0100

    Setup Server
    Setup send and recive event for placement of actors

---
+++ server.js
@@ -0,0 +1,40 @@
+const { instrument } = require("@socket.io/admin-ui");
+
+// ***** Admin Panel *****
+/*
+1. Go to:
+    https://admin.socket.io/
+2. Login with
+    http://localhost:3000
+3. Volla done!
+
+ */
+
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: ["http://localhost:5173", "https://admin.socket.io"],
+        credentials: true,
+    },
+})
+
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    // Place Actor
+    socket.on("send-actor-placement", (jsondata) => {
+        console.log(`json inhalt: ${jsondata}`)
+        socket.broadcast.emit("receive-actor-placement", jsondata)
+    })
+
+
+
+})
+
+instrument(io, {
+    auth: false,
+    mode: "development",
+});
+
+
+
